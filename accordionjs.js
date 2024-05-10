af5f2e66bb8f9e53c726400a6c083e0292f55a4b
YOUSUF PESHAVARIA <45356333+ypesh@users.noreply.github.com>
2024-05-10 06:39:54 +0100

    Add files via upload

---
+++ accordionjs.js
@@ -0,0 +1,15 @@
+//function javascripts accordionjs.js based on https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion_symbol
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
